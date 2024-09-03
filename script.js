003387668c9b6880530b00f2cdde6e0cb6756006
Kalinina Oksana <ksan4ik92@gmail.com>
2024-09-03 14:22:29 +0300

    f

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input")
+const span = document.querySelector('span');
+const card = document.querySelector('.card')
+
+input.addEventListener("focus", (e)=> {
+    const heightViewport = window.visualViewport.height;
+    const heightDevice = document.body.clientHeight;
+    span.textContent=`Viewport: ${heightViewport}, device: ${heightDevice}`
+})
